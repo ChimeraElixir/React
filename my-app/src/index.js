5100c96800fd34b6b3d27ef687139cf22f1768c4
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-06-23 22:37:57 +0530

    Added Index.js

---
+++ my-app/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const BookList = () => {
+
+    return (
+      <section>
+        <Book></Book>
+        <Book></Book>
+      </section>
+    )
+};
+
+const Book = () => {
+    return (
+        <article>
+            <Image/>
+            <Title/>
+            <Author/>
+        </article>
+    );
+};
+
+const Image = () =>  <h2>Image</h2> ;
+
+const Title = () =>  <h2>Title</h2> ;
+
+const Author = () =>  <h2>Author</h2> ;
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(<BookList/>);
