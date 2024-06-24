85963b94f3562932685ff961a5f53348daeea566
Gaurav Verma <gaurav94266@gmail.com>
2024-06-24 19:09:53 +0530

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,31 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './index.css';
+
+const firstBook ={
+  image:'https://m.media-amazon.com/images/I/419aJfhczCL._SY445_SX342_.jpg',
+  title:'Atomic habit',
+  author:'James Clear',
+}
+const secondBook ={
+  image:'https://m.media-amazon.com/images/I/41656QTKoZL._SY445_SX342_.jpg',
+  title:'King of Sloth',
+  author:'Ana Huang',
+}
+
 const BookList = () => {
 
     return (
-      <section>
-        <Book></Book>
-        <Book></Book>
+      <section className='booklist'>
+        <Book image={firstBook.image} title={firstBook.title} author={firstBook.author}></Book>
+        <Book image={secondBook.image} title={secondBook.title} author={secondBook.author}></Book>
+        
       </section>
     )
+
 };
 
-const Book = () => {
-    return (
-        <article>
-            <Image/>
-            <Title/>
-            <Author/>
-        </article>
-    );
+const Book = (props) => {
+  return (
+      <article className='book'>
+          <img src={props.image} alt={props.title}></img>
+          <h2>{props.title}</h2>
+          <h4>{props.author}</h4>
+      </article>
+  );
 };
 
-const Image = () =>  <h2>Image</h2> ;
 
-const Title = () =>  <h2>Title</h2> ;
-
-const Author = () =>  <h2>Author</h2> ;
 
 
 
