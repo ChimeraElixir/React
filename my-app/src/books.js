dabfd24aecd5c443130d864f46118517b2d15ba6
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-07-08 18:26:37 +0530

    Learned index

---
+++ my-app/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/419aJfhczCL._SY445_SX342_.jpg",
+    title: "Atomic habit",
+    author: "James Clear",
+    id: 1,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/41656QTKoZL._SY445_SX342_.jpg",
+    title: "King of Sloth",
+    author: "Ana Huang",
+    id: 2,
+  },
+]
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {books} from './books'
+import Book from './Book'
 
 import './index.css';
 
-const books=[
-    {
-      img:'https://m.media-amazon.com/images/I/419aJfhczCL._SY445_SX342_.jpg',
-      title:'Atomic habit',
-      author:'James Clear',
-      id:1,
-    },
-    {
-      img:'https://m.media-amazon.com/images/I/41656QTKoZL._SY445_SX342_.jpg',
-      title:'King of Sloth',
-      author:'Ana Huang',
-      id:2,
-    }
-];
+
 
 const BookList = () => {
 
     return (
       <section className='booklist'>
-        <ExamppleEventHandler/>
+        
         {
-          books.map((book)=>{
-            return <Book {...book} />;
+          books.map((book,index)=>{
+            return <Book {...book}  number={index} />;
           })
         }
         
@@ -34,37 +23,6 @@ const BookList = () => {
 
 };
 
-const ExamppleEventHandler = () => {
-  const handleForm = () => {
-    return("On form Change");
-  };
-  const handleButton = () => {
-    alert("On Button");
-  }
-  return(
-    <section>
-      <form>
-        <h2>Form Input</h2>
-        <input type='text' 
-        name='example' 
-        onChange={handleForm} 
-        style={{ margin: '1rem 0' }}/>
-      </form>
-      <button onClick={handleButton}>Click Me</button>
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const {img,title,author}=props;
-  return (
-      <article className='book'>
-          <img src={img} alt={title}></img>
-          <h2>{title}</h2>
-          <h4>{author}</h4>
-      </article>
-  );
-};
 
 
 
