31544768a5121811c6a98b1ae31f6326da90856e
Gaurav Verma <gaurav94266@gmail.com>
2024-07-19 00:58:04 +0530

    3- conditional functioning

--- 3-advance-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 3-advance-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,30 @@
+import { useEffect,useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [state,setState]=useState(false);
+
+  const handleClick = ()=>{
+    if(state){
+      setState(false);
+      return
+    }
+    setState(true);
+    // setState(state ^ true) Problem it display 0 when false
+  }
+
+  return (
+    <>
+    <h1>Toggle Challange</h1>
+    <button type="button" value={state} className="btn" onClick={handleClick}>Toggle</button>
+    {/* <button type="button" value={state} className="btn" onClick={()=> setState(!state)}>Toggle</button> */}
+    
+    {state && <Alert/>}
+    </>
+  );
 };
+
+const Alert=()=>{
+  return <div className="alert alert-danger">Hello world</div>
+}
 
 export default ToggleChallenge;
--- 3-advance-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 3-advance-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const Login = () => {
+    setUser({ name: "gaurav" });
+  };
+  const Logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello there,{user.name}</h4>
+          <button type='button' className='btn' onClick={Logout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button type='button' className='btn' onClick={Login}>
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
