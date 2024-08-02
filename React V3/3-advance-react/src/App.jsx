7feb31882602124ff6db3b76cacf31dd80c63cf7
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-08-02 12:51:35 +0530

    global context example

--- React V3/3-advance-react/src/App.jsx
+++ React V3/3-advance-react/src/App.jsx
@@ -1,14 +1,11 @@
-// import Starter from './tutorial/08-custom-hooks/starter/02-fetch-data'
-import Navbar1 from './tutorial/09-context-api/starter/Navbar'
-import Navbar from './tutorial/09-context-api/final/prop-drilling/Navbar'
+import ReducerBasics from "./tutorial/10-useReducer/starter/01-useReducer"
 
 
 
 function App() {
   return (
-    <div className="">
-      <Navbar1/>
-      <Navbar/>
+    <div className="container">
+      <ReducerBasics/>
     </div>
   )
 }
