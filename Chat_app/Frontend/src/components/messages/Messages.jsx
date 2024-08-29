ed67135178397f4363743bee4ffac74e71cbd002
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-08-29 07:21:57 +0530

    completed

--- Chat_app/Frontend/src/components/messages/Messages.jsx
+++ Chat_app/Frontend/src/components/messages/Messages.jsx
@@ -3,16 +3,26 @@ import useConversation from "../../zustand/useConversation"
 import Message from "./Message"
 import useGetMessages from "../../hooks/useGetMessages"
 import MessageSkeleton from "../skeleton/MessageSkeleton"
+import { useEffect, useRef } from "react"
 
 const Messages = () => {
   const { messages, loading } = useGetMessages()
+  const lastMessage = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessage.current?.scrollIntoView({ behavior: "smooth" })
+    }, 100)
+  }, [messages])
 
   return (
     <div className="px-4 flex-1 overflow-auto">
       {!loading &&
         messages.length > 0 &&
         messages.map((message) => (
-          <Message key={message._id} message={message} />
+          <div key={message._id} ref={lastMessage}>
+            <Message message={message} />
+          </div>
         ))}
       {loading &&
         [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
--- Chat_app/Frontend/src/components/sidebar/SearchInput.jsx
+++ Chat_app/Frontend/src/components/sidebar/SearchInput.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import {IoSearchSharp} from 'react-icons/io5'
+import React, { useState } from "react"
+import { IoSearchSharp } from "react-icons/io5"
+import useConversation from "../../zustand/useConversation"
+import useGetConversations from "../../hooks/useGetConversations"
 
 const SearchInput = () => {
-  return (
-    <form  className='flex items-center gap-2'>
-        <input type='text' placeholder='Search...' className='input input-bordered rounded-full'></input>
-        <button type='submit' className='btn btn-circle bg-sky-400 text-white'>
-            <IoSearchSharp className="w-6 h-6 outline-none"/>
-        </button>
+  const [search, setSearch] = useState("")
+  const { setSelectedConversation } = useConversation()
+  const { conversations } = useGetConversations()
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!search) return
+    if (search.length < 3) {
+      toast.error("Please enter at least 3 characters")
+      return
+    }
+    const conversation = conversations.find((conversation) =>
+      conversation.fullName.toLowerCase().includes(search.toLowerCase())
+    )
+    if (conversation) {
+      setSelectedConversation(conversation)
+      setSearch("")
+    } else {
+      toast.error("No User Found")
+    }
+  }
+
+  return (
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+      <button type="submit" className="btn btn-circle bg-sky-400 text-white">
+        <IoSearchSharp className="w-6 h-6 outline-none" />
+      </button>
     </form>
   )
 }
